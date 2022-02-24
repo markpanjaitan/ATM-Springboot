72e7a2f25271a61bb5894a5d0c9f48564d96ec39
markpanjaitan <mark.panjaitan@gmail.com>
2022-02-24 23:00:10 +0700

    update DDL.sql

--- src/main/resources/DDL.sql
+++ src/main/resources/DDL.sql
@@ -3,18 +3,21 @@ USE test;
 CREATE TABLE `customer` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`nama` VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
-	`balance` BIGINT(20) NULL DEFAULT NULL,
+	`balance` BIGINT(20) NOT NULL,
 	`owed_to_id` INT(11) NULL DEFAULT NULL,
-	`owed_nominal` BIGINT(20) NULL DEFAULT NULL,
+	`owed_to_nominal` BIGINT(20) NULL DEFAULT NULL,
+	`keterangan` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
 	PRIMARY KEY (`id`) USING BTREE,
 	UNIQUE INDEX `nama` (`nama`) USING BTREE
 )
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=2
+AUTO_INCREMENT=6
 ;
 
-INSERT INTO `customer` (`id`, `nama`, `balance`, `owed_to_id`, `owed_nominal`) VALUES (1, 'valentino', 5066000, NULL, NULL);
-INSERT INTO `customer` (`id`, `nama`, `balance`, `owed_to_id`, `owed_nominal`) VALUES (2, 'donny', 605000, NULL, NULL);
-INSERT INTO `customer` (`id`, `nama`, `balance`, `owed_to_id`, `owed_nominal`) VALUES (3, 'bayu', 1200000, NULL, NULL);
-INSERT INTO `customer` (`id`, `nama`, `balance`, `owed_to_id`, `owed_nominal`) VALUES (4, 'abet', 440000, NULL, NULL);
+
+INSERT INTO `customer` (`id`, `nama`, `balance`, `owed_to_id`, `owed_to_nominal`, `keterangan`) VALUES (1, 'valentino', 5066000, NULL, NULL, NULL);
+INSERT INTO `customer` (`id`, `nama`, `balance`, `owed_to_id`, `owed_to_nominal`, `keterangan`) VALUES (2, 'donny', 605000, NULL, NULL, NULL);
+INSERT INTO `customer` (`id`, `nama`, `balance`, `owed_to_id`, `owed_to_nominal`, `keterangan`) VALUES (3, 'bayu', 1200000, 2, 500000, NULL);
+INSERT INTO `customer` (`id`, `nama`, `balance`, `owed_to_id`, `owed_to_nominal`, `keterangan`) VALUES (4, 'abet', 440000, 1, 100000, NULL);
+INSERT INTO `customer` (`id`, `nama`, `balance`, `owed_to_id`, `owed_to_nominal`, `keterangan`) VALUES (5, 'vincent', 550000, NULL, NULL, NULL);
